e67cc7eb0662cc97136a087389c6f0645d449deb
Odintsov Sergey <loveravel@yandex.ru>
2018-11-27 23:29:16 +0400

    Добавлен вывод пинов

--- js/map.js
+++ js/map.js
@@ -53,6 +53,41 @@ function checkForIdentical(arr, val) {
   return false;
 }
 
+function getAvatarPath(index) {
+  return 'img/avatars/user0' + index + '.png';
+}
+
+function getLocation() {
+  return {
+    x: getRandom(0, 1200) - 25,
+    y: getRandom(130, 630) - 70
+  };
+}
+
+function getAddress() {
+  return getLocation().x + ', ' + getLocation().y;
+}
+
+function getFeatures(optionsFeaturesList) {
+  var featuresList = [];
+  for (var i = 0; i < optionsFeaturesList.length; i++) {
+    var featureIndex = getRandom(0, optionsFeaturesList.length);
+    if (!checkForIdentical(featuresList, optionsFeaturesList[featureIndex])) {
+      featuresList += optionsFeaturesList[featureIndex];
+    }
+  }
+  return featuresList;
+}
+
+function getPhotosArray() {
+  var photos = [];
+  for (var i = 0; i < 3; i++) {
+    var j = getRandom(1, 3);
+    photos.push('http://o0.github.io/assets/images/tokyo/hotel' + j + '.jpg');
+  }
+  return photos;
+}
+
 function Author(avatar) {
   this.avatar = avatar;
 }
@@ -76,90 +111,86 @@ function Location(x, y) {
   this.y = y;
 }
 
-var getCardList = function () {
-  var cardList = [];
+var getAuthorOptionList = function () {
+  var authorOptionList = [];
   for (var i = 0; i < 8; i++) {
-    cardList.push(/*new Author(getAvatarPath(i),*/
-        new Offer(
-            OptionsCard.TITLE_LIST[i],
-            getAddress(),
-            getRandom(1000, 1000000),
-            OptionsCard.TYPE_LIST[getRandom(0, OptionsCard.TYPE_LIST.length - 1)],
-            getRandom(1, 5),
-            OptionsCard.CHECKIN_LIST[getRandom(0, OptionsCard.CHECKIN_LIST.length - 1)],
-            OptionsCard.CHECKOUT_LIST[getRandom(0, OptionsCard.CHECKOUT_LIST.length - 1)],
-            getFeatures(OptionsCard),
-            OptionsCard.DESCRIPTION_LIST,
-            getPhotosArray()));
-        /*new Location(
-            getLocation().x,
-            getLocation().y)));*/
+    authorOptionList.push(new Author(getAvatarPath(getRandom(1, 8))));
   }
-  return cardList;
+  return authorOptionList;
 };
 
-function getAvatarPath(index) {
-  return 'img/avatars/user' + index + '.png';
-}
-
-function getLocation() {
-  return {
-    x: getRandom(0, 1200),
-    y: getRandom(130, 630)
-  };
-}
-
-function getAddress() {
-  return getLocation().x + ', ' + getLocation().y;
-}
-
-function getFeatures(optionsFeaturesList) {
-  var featuresList = [];
-  for (var i = 0; i < optionsFeaturesList.length; i++) {
-    var featureIndex = getRandom(0, optionsFeaturesList.length);
-    if (!checkForIdentical(featuresList, optionsFeaturesList[featureIndex])) {
-      featuresList += optionsFeaturesList[featureIndex];
-    }
+var getOfferOptionList = function () {
+  var offerOptionList = [];
+  for (var i = 0; i < 8; i++) {
+    offerOptionList.push(new Offer(
+        OptionsCard.TITLE_LIST[getRandom(0, 7)],
+        getAddress(),
+        getRandom(1000, 1000000),
+        OptionsCard.TYPE_LIST[getRandom(0, OptionsCard.TYPE_LIST.length - 1)],
+        getRandom(1, 5),
+        getRandom(1, 10),
+        OptionsCard.CHECKIN_LIST[getRandom(0, OptionsCard.CHECKIN_LIST.length - 1)],
+        OptionsCard.CHECKOUT_LIST[getRandom(0, OptionsCard.CHECKOUT_LIST.length - 1)],
+        getFeatures(OptionsCard),
+        OptionsCard.DESCRIPTION_LIST,
+        getPhotosArray()));
   }
-  return featuresList;
-}
+  return offerOptionList;
+};
 
-function getPhotosArray() {
-  var i = [];
-  var photos = [];
-  while (photos.length < 2) {
-    var j = getRandom(1, 3);
-    if (!checkForIdentical(i, j)) {
-      photos += 'http://o0.github.io/assets/images/tokyo/hotel' + j + '.jpg';
-      i += j;
-    }
+var getLocationOptionList = function () {
+  var locationOptionList = [];
+  for (var i = 0; i < 8; i++) {
+    locationOptionList.push(new Location(
+        getLocation().x,
+        getLocation().y));
   }
-  return photos;
-}
+  return locationOptionList;
+};
 
 document.querySelector('.map').classList.remove('map--faded');
 
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
-var mapElement = document.querySelector('.map');
+var map = document.querySelector('.map');
 var filtersContainer = document.querySelector('.map__filters-container');
 
-function renderCard(card) {
+function renderCard(author, offer) {
   var cardElement = cardTemplate.cloneNode(true);
-  //cardElement.querySelector('.popup_avatar').src = card.avatar;
-  cardElement.querySelector('.popup__title').textContent = card.title;
-  cardElement.querySelector('.popup__text--address').textContent = card.address;
-  cardElement.querySelector('.popup__text--price').textContent = card.price + '&#x20db;';
-  cardElement.querySelector('.popup__type').textContent = card.type;
-  cardElement.querySelector('.popup__text--capacity').textContent = card.rooms + ' комнат(ы) для '
-    + card.guests + ' гостей';
-  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.checkin
-    + ', выезд до' + card.checkout;
+  cardElement.querySelector('.popup__avatar').src = author.avatar;
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = offer.type;
+  cardElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнат(ы) для '
+    + offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin
+    + ', выезд до ' + offer.checkout;
   cardElement.querySelector('.popup__features');
-  cardElement.querySelector('.popup__description').textContent = card.description;
-  //cardElement.querySelector('.popup__photos').src = card.photos[0];
+  cardElement.querySelector('.popup__description').textContent = offer.description;
+  cardElement.querySelector('.popup__photos').src = offer.photos[0];
   return cardElement;
 }
 
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+function renderMapPin(author, offer, location) {
+  var mapPinElement = mapPinTemplate.cloneNode(true);
+  mapPinElement.style.left = location.x + 'px';
+  mapPinElement.style.top = location.y + 'px';
+  mapPinElement.querySelector('img').src = author.avatar;
+  mapPinElement.querySelector('img').alt = offer.title;
+  return mapPinElement;
+}
+
 var fragment = document.createDocumentFragment();
-fragment.appendChild(renderCard(getCardList()[0]));
-mapElement.appendChild(fragment);
+fragment.appendChild(
+    renderCard(getAuthorOptionList()[0],
+        getOfferOptionList()[0]));
+for (var i = 0; i < 8; i++) {
+  fragment.appendChild(
+      renderMapPin(
+          getAuthorOptionList()[i],
+          getOfferOptionList()[i],
+          getLocationOptionList()[i]));
+}
+map.insertBefore(fragment, filtersContainer);
