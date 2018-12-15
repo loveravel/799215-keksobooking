f7e531c7faddd11ecac13af3cda83d1859701c84
Odintsov Sergey <loveravel@yandex.ru>
2018-12-16 03:41:21 +0400

    Добавил валидацию связки полей цена + тип жилья

--- js/form.js
+++ js/form.js
@@ -14,10 +14,33 @@
     ];
     if (numberOfRoomField.value === '100' && numberOfGuestField.value !== '0') {
       numberOfGuestField.setCustomValidity(texts[0]);
-    } else if (numberOfRoomField.value < numberOfGuestField.value) {
+    } else if (numberOfRoomField.value < numberOfGuestField.value && numberOfGuestField.value !== '100') {
       numberOfGuestField.setCustomValidity(texts[numberOfRoomField.value]);
     } else {
       numberOfGuestField.setCustomValidity('');
+    }
+  }
+
+  var typeOfHousing = document.querySelector('#type');
+  var price = document.querySelector('#price');
+
+  function validateTypeAndPrice() {
+    var texts = [
+      'Минимальная цена для типа жилья "Бунгало" 0 рублей',
+      'Минимальная цена для типа жилья "Квартира" 1000 рублей',
+      'Минимальная цена для типа жилья "Дом" 5000 рублей',
+      'Минимальная цена для типа жилья "Дворец" 10000 рублей'
+    ];
+    if (typeOfHousing.value === 'bungalo' && price.value < 0) {
+      price.setCustomValidity(texts[0]);
+    } else if (typeOfHousing.value === 'flat' && price.value < 1000) {
+      price.setCustomValidity(texts[1]);
+    } else if (typeOfHousing.value === 'house' && price.value < 5000) {
+      price.setCustomValidity(texts[2]);
+    } else if (typeOfHousing.value === 'palace' && price.value < 10000) {
+      price.setCustomValidity(texts[3]);
+    } else {
+      price.setCustomValidity('');
     }
   }
 
@@ -27,6 +50,25 @@
 
   numberOfGuestField.addEventListener('change', function () {
     validateRoomAndGuest();
+  });
+
+  typeOfHousing.addEventListener('change', function () {
+    validateTypeAndPrice();
+    if (typeOfHousing.value === 'bungalo') {
+      price.placeholder = '0';
+    } else if (typeOfHousing.value === 'flat') {
+      price.placeholder = '1000';
+    } else if (typeOfHousing.value === 'house') {
+      price.placeholder = '5000';
+    } else if (typeOfHousing.value === 'palace') {
+      price.placeholder = '10000';
+    } else {
+      price.setCustomValidity('');
+    }
+  });
+
+  price.addEventListener('change', function () {
+    validateTypeAndPrice();
   });
 
   form.addEventListener('submit', function (evt) {
